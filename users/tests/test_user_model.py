3c1f257d88cb58a4a9d782b77152e13a9d532b86
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2021-01-06 20:56:28 -0300

    Adiciona projeto inicial

---
+++ users/tests/test_user_model.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from ..models import User
+
+
+class UserModelTestCase(TestCase):
+    def test_if_can_add_a_email_based_user(self):
+        user = User(email="my_email@email.com")
+        user.set_password('my_password')
+        user.save()
+
+        self.assertTrue(User.objects.exists())
