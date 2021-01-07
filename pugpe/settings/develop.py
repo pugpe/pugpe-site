fa5c4bcfd6a4a49122991fa2e8eae68367f94fa3
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2021-01-07 20:26:22 -0300

    Ajusta settings

--- pugpe/settings/develop.py
+++ pugpe/settings/develop.py
@@ -10,4 +10,4 @@ ALLOWED_HOSTS = ["*"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
