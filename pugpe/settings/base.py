1d2a17fd70772140ec33dbad176af030cfb58fc3
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2021-01-07 21:16:53 -0300

    Ajusta commits
    Ajusta settings de produção
    
    Adiciona psycopg2
    
    Ajusta settings
    
    Ajusta settings
    
    Renomeia settings.py para base.py
    
    Adiciona DEV_APPS

--- pugpe/settings/base.py
+++ pugpe/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+DEV_APPS = [
+    "test_without_migrations",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
