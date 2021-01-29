cfc7fb466b58066eaa723c5cf62d467f76538a76
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2021-01-29 19:12:10 -0300

    Adiciona model MeetingEvent

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Meeting
+from .models import Meeting, MeetingEvent
 
 
 admin.site.register(Meeting)
+admin.site.register(MeetingEvent)
--- pugpe/settings/base.py
+++ pugpe/settings/base.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 DEV_APPS = [
     "test_without_migrations",
+    "debug_toolbar",
 ]
 
 INSTALLED_APPS = [
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third Party Apps
     "test_without_migrations",
+    "debug_toolbar",
     # Project's Apps
     "common",
     "users",
@@ -118,9 +120,9 @@ AUTH_USER_MODEL = "users.User"
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Recife"
 
 USE_I18N = True
 
